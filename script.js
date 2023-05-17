cbc29a9f3f4140fbb3f890d51289df4328d916b9
Sreelakshmi E P Veedu <130582822+SreelakshmiEP@users.noreply.github.com>
2023-05-17 11:05:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function validation(){
+    if(document.getElementById("name-input").value=="")
+    {
+        document.getElementById("name-validation").style.display="block";
+    }
+    if(document.getElementById("email-input").value=="")
+    {
+        document.getElementById("email-validation").style.display="block";
+    }
+    if(document.getElementById("message-input").value=="")
+    {
+        document.getElementById("message-validation").style.display="block";
+    }
+    return false;
+}
+
